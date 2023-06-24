d8c2c7d36148b77b4026c3b59f44dddc5acb3831
imran116 <ih9114101@gmail.com.com>
2023-06-24 20:57:00 +0600

    Add Edit and Delete for

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.HomeView.as_view()),
-    path('category/add', views.AddCategoryView.as_view(), name='add-category'),
-    path('category/list', views.ListCategoryView.as_view(), name='list-category')
+    path('category/add/', views.AddCategoryView.as_view(), name='add-category'),
+    path('category/list/', views.ListCategoryView.as_view(), name='list-category'),
+    path('category/edit-category/<int:pk>/', views.EditCategoryView.as_view(), name='edit-category'),
+    path('category/delete-category/<int:pk>/', views.DeletCategoryView.as_view(), name='delete-category'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView
 
 from inventory import forms
 from inventory.models import Category
@@ -21,3 +22,18 @@ class ListCategoryView(ListView):
     template_name = 'list_category.html'
     queryset = Category.objects.all()
     context_object_name = 'categoryObj'
+
+
+class EditCategoryView(UpdateView):
+    form_class = forms.CategoryForm
+    template_name = 'edit-category.html'
+    queryset = Category.objects.all()
+    success_url = '/category/list/'
+    pk_url_kwarg = 'pk'
+
+
+class DeletCategoryView(View):
+    def get(self, request, pk):
+        instance = Category.objects.get(pk=pk)
+        instance.delete()
+        return redirect('/category/list/')
